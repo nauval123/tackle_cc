58cd8ce09cbb9a91ed2ff2fa26131ee725c8c8c1
nauval123 <nayuya24@gmail.com>
2020-11-17 13:01:29 +0700

    membuat tampilan homepage, navbar, listview,form login

--- lib/detail_mission.dart
+++ lib/detail_mission.dart
@@ -1,1 +1,63 @@
 import 'package:flutter/material.dart';
+
+class detailmissionPage extends StatelessWidget {
+   final String jeneng= 'Beli Botol Minum Tidak Sekali Pakai';
+  final String ttl='Easy';
+  final String bio= 'Indonesia adalah penyumbang sampah plastik kedua terbanyak didunia! Kebanyakan plastik dipakai hanya beberapa kali saja, kemudian dibuang begitu saja ke TPA atau diolah dengan cara didaur ulang atau dibakar dengan insinerator.Daur ulang memang memiliki dampak lingkungan yang paling minim, namun tidak semua plastik dapat di daur ulang, terlebih plastik dengan kualitas yang rendah. Daur ulang plastik juga memerlukan energi yang besar, sehingga menghasilkan emisi karbon yang cukup tinggi.';
+ 
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.green[400],
+        title: Text(this.jeneng,
+        style: TextStyle(
+          fontSize: 13,
+        ),),
+        centerTitle: true,
+      ),
+      body: Card(
+        elevation: 8,
+        margin: EdgeInsets.all(20),
+        child:Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children:<Widget>[
+          Image.network(
+          'https://img.freepik.com/free-vector/bottle-water-concept-illustration_114360-2183.jpg?size=338&ext=jpg',
+          height: 200.0,
+          width: 162.0,
+          fit: BoxFit.fill,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              Column(
+                children: <Widget>[
+                  Container(
+                     padding: EdgeInsets.only(left: 50,right: 50,bottom: 20,top:10),
+                     child:Text(this.ttl,
+                  style: TextStyle(
+                    color:Colors.grey,
+                    fontSize: 20.0,                    
+                  ),
+                  ) ,
+                  ),
+                ],
+              ),
+            ],   
+          ),
+          Container(
+            padding: EdgeInsets.only(left: 50,right: 50,bottom: 20,top:10),
+            child: Text(
+              this.bio,
+              style: TextStyle(
+                fontSize: 15.0,
+              ),
+            ),
+          ),
+        ],
+      ),
+    ),
+    );
+  }
+}
--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,15 +1,126 @@
 import 'package:flutter/material.dart';
+import 'package:tackle_cc/daftarMisi.dart';
 
-class Homepage extends StatelessWidget{
+import 'detail_mission.dart';
+
+
+
+class HomePage extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
- return Scaffold(
-   appBar: AppBar(
-     title: Text("Ini testing"),
-   ),
-   body: Center(
-     child: Text("halaman login!"),
-   ),
- );
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+ int _selectedIndex = 0;
+static const TextStyle optionStyle = TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+static  List<Object> _widgetOptions = <Object>[
+  //buat tampilan tiap halaman
+   misiygSelesai(),
+  kategoriMisi(),
+  Text(
+     'Profil dan tentang aplikasi',
+     style: optionStyle,
+  ),
+];
+
+void _onItemTapped(int index) {
+  setState(() {
+    _selectedIndex = index;
+  });
+}
+
+@override
+Widget build(BuildContext context) {
+  return Scaffold(
+    body: Center(
+      //memanggil dari list object kelas di atas untuk tampilan tiap halaman
+      child: _widgetOptions.elementAt(_selectedIndex),
+    ),
+    bottomNavigationBar: BottomNavigationBar(
+      items: const <BottomNavigationBarItem>[
+        BottomNavigationBarItem(
+          icon: Icon(Icons.home_filled),
+          title: Text("Home") ,
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.whatshot_outlined),
+          title: Text('Misi'),
+        ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_circle),
+            title: Text("Tentang")
+          ),
+        ],
+        unselectedItemColor: Colors.black54,
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.green[400],
+        onTap: _onItemTapped,
+      ),
+    );
   }
 }
+
+
+class misiygSelesai extends StatefulWidget {
+  @override
+  _misiygSelesaiState createState() => _misiygSelesaiState();
+}
+
+class _misiygSelesaiState extends State<misiygSelesai> {
+   //listview untuk tampilan misi
+  List <String> misiSelesai =['Menggunakan Botol pribadi', 'Membuang sampah ditempatnya 3x', 'Menanam Pohon'];
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.green[400],
+          centerTitle: true,
+          title: Text('Misi Selesai'),
+          leading: Container(),
+        ),
+        body: ListView.builder(
+          padding: EdgeInsets.all(10.0),
+          itemCount: misiSelesai.length,
+          itemBuilder: (context, i) {
+            return Column(
+              children: <Widget>[
+              Container(
+                margin: EdgeInsets.all(8.0),
+                child: Center(
+                  child: cardDetail(misiSelesai[i])
+                ),
+            ),
+              ],
+            ); 
+          },
+        ),
+      );
+  }
+
+  Widget cardDetail(title){
+    return Card(
+    margin: EdgeInsets.all(5),
+    elevation: 5,
+    child: ListTile(
+      leading: Icon(Icons.accessibility_new_rounded,color: Colors.green,),
+      title: Text(
+        title,
+        style: TextStyle(fontSize: 12,
+        fontWeight: FontWeight.bold),
+      ),
+      subtitle: Text('easy'),
+      trailing: Container(
+        child: FlatButton.icon( 
+          onPressed: (){
+            Navigator.push(context, MaterialPageRoute(builder: (BuildContext context) =>detailmissionPage()));
+          }, 
+          icon: Icon(Icons.arrow_forward_ios_rounded,color: Colors.green,),
+           label: Text('detail',style: TextStyle(color: Colors.green),),
+      )
+      ),      
+    )
+    );
+  }
+}
+
+
